f5c7040b5eb3f06657c27022b7e4408f318a2268
yuzuhakuon <yuzuhakuon@gmail.com>
2021-12-14 16:42:59 +0800

    build: build tflite

---
+++ build_tflite.ps1
@@ -0,0 +1,22 @@
+function BuildForWindows($platform, $build_type) {
+    $build_dir = "build"
+    mkdir $build_dir -Force -ErrorAction Stop | Out-Null
+    cd $build_dir
+    pwd
+
+    if ($platform -eq "x64") {
+        $msbuild_platform = "x64"
+    }
+    else {
+        $msbuild_platform = "Win32"
+    }
+
+
+
+    cmake -G "Visual Studio 16 2019" -A $msbuild_platform ../tensorflow/lite/c
+    cmake --build . --config Release
+
+    # msbuild INSTALL.vcxproj /t:build /p:configuration=$build_type /p:platform=$msbuild_platform -maxcpucount
+    ls
+    cd ..
+}
